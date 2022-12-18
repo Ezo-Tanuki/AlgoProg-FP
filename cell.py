533a2a025399b004f1d4b87f73de464c5f1eb59c
Ëzo-Tanuki <michailbryanlie@gmail.com>
2022-12-18 14:31:04 +0700

    add line object

--- cell.py
+++ cell.py
@@ -9,8 +9,8 @@ class Cell():
 
         self.index = row * column + column
         self.rect = pygame.Rect(
-            (s.padding_x + column*s.cellsize, s.padding_y + row*s.cellsize),
-            (s.cellsize, s.cellsize)) #make a square object
+            s.padding_x + column*s.cellsize, s.padding_y + row*s.cellsize,
+            s.cellsize, s.cellsize) #make a square object
         
         #stores top&bottom y coor. and left&right x coor. order = top right bottom left
         self.sides = (self.rect.top, self.rect.right, self.rect.bottom, self.rect.left) 
@@ -21,4 +21,23 @@ class Cell():
                     (self.sides[1], self.sides[2]), #bottom-right
                     (self.sides[3], self.sides[2])) #bottom-left
         
-        self.line = [False] * 4 #up right down left
+        self.line_object = [   
+            pygame.Rect(self.rect.left + s.dot_radius,self.rect.top - s.dot_radius, s.cellsize - s.dot_radius * 2, s.dot_radius * 2),
+            pygame.Rect(self.rect.right - s.dot_radius, self.rect.top + s.dot_radius, s.dot_radius * 2, s.cellsize - s.dot_radius * 2),
+            pygame.Rect(self.rect.left + s.dot_radius,self.rect.bottom - s.dot_radius, s.cellsize - s.dot_radius * 2, s.dot_radius * 2),
+            pygame.Rect(self.rect.left - s.dot_radius, self.rect.top + s.dot_radius, s.dot_radius * 2, s.cellsize - s.dot_radius * 2)
+            ] #up right down left
+
+        self.line = [True, True, True, True] #up right down left
+        self.claim = None #
+    
+    def updateLines(self, screen):
+        for index, side in enumerate(self.line):
+            if side:
+                pygame.draw.line(screen, (0, 0, 0), self.corner[index], self.corner[(index+1)% 4], 2)
+
+    def displayLineObject(self, screen):
+        for side in self.line_object:
+            if side:
+                pygame.draw.rect(screen, (0, 0, 0), side)
+                
