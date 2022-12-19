35ab700f8545a0ae66da193d43c64786aeba8125
Ëzo-Tanuki <michailbryanlie@gmail.com>
2022-12-19 11:30:03 +0700

    set column to 10

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ class Settings():
         self.screen_width = 600
         self.screen_height = 600
 
-        self.columns = self.rows = 20
+        self.columns = self.rows = 10
         
-        self.cellsize = min(40, min(self.screen_width, self.screen_height) / (self.columns+1))
+        self.cellsize = min(100, min(self.screen_width, self.screen_height) / (self.columns+1))
         self.padding_x = (self.screen_width - self.rows * (self.cellsize)) / 2
         self.padding_y = (self.screen_width - self.columns * (self.cellsize)) / 2
 
