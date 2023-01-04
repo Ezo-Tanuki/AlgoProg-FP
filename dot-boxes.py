27a6171f300124c677b006608c75823babcb7e19
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-01-04 23:05:59 +0700

    add colors

--- dot-boxes.py
+++ dot-boxes.py
@@ -8,23 +8,27 @@ from settings import Settings
 from stats import Stats
 from button import Button
 
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
 def main():
     pygame.init()
     pygame.display.set_caption("Dot and Boxes")
-
     
     settings = Settings()
+    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     
-    cells = [Cell(row, column) for row in range(settings.rows)
+    
+    
+    cells = [Cell(row, column, screen, settings) for row in range(settings.rows)
         for column in range(settings.columns)] #render cell object
-    players = (Player((255, 0, 0)), Player((0, 0, 255)))
+    players = (Player(RED), Player(BLUE))
     
     current_player = [players[0], 0]
-    screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+    
     stats = Stats(screen, players, settings)
     button = Button(screen, "Retry", settings)
     run = True
-    print(cells[0].rect)
     while run:
         gf.update_screen(settings, screen, cells, players, stats, button)
         gf.checkEvents(settings, screen, cells, players, current_player, stats, button)
