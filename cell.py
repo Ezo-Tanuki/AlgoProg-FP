ac65270aadb2eff92b11024859011bd0951043a4
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-01-08 20:06:06 +0700

    add settings

--- cell.py
+++ cell.py
@@ -1,18 +1,20 @@
 import pygame
 from settings import Settings
+from player import Player
 
 
 class Cell():
-    def __init__(self, row, column, screen, s = Settings()):
+    def __init__(self, row : int, column : int, screen : pygame.Surface, settings : Settings) -> None:
         self.row = row
         self.column = column
 
         self.screen = screen
+        self.settings = settings
 
         self.index = row * column + column
         self.rect = pygame.Rect(
-            s.padding_x + column*s.cellsize, s.padding_y + row*s.cellsize,
-            s.cellsize, s.cellsize) #make a square object
+            self.settings.padding_x + column*self.settings.cellsize, self.settings.padding_y + row*self.settings.cellsize,
+            self.settings.cellsize, self.settings.cellsize) #make a square object
         
         #stores top&bottom y coor. and left&right x coor. order = top right bottom left
         self.sides = (self.rect.top, self.rect.right, self.rect.bottom, self.rect.left) 
@@ -23,40 +25,44 @@ class Cell():
                     (self.sides[1], self.sides[2]), #bottom-right
                     (self.sides[3], self.sides[2])) #bottom-left
         
-        self.line_object = [   
-            pygame.Rect(self.rect.left + s.dot_radius, self.rect.top - s.dot_radius, s.cellsize - s.dot_radius * 2, s.dot_radius * 2),
-            pygame.Rect(self.rect.right - s.dot_radius, self.rect.top + s.dot_radius, s.dot_radius * 2, s.cellsize - s.dot_radius * 2),
-            pygame.Rect(self.rect.left + s.dot_radius,self.rect.bottom - s.dot_radius, s.cellsize - s.dot_radius * 2, s.dot_radius * 2),
-            pygame.Rect(self.rect.left - s.dot_radius, self.rect.top + s.dot_radius, s.dot_radius * 2, s.cellsize - s.dot_radius * 2)
-            ] #up right down left
+        self.line_object = (   
+            pygame.Rect(self.rect.left + self.settings.dot_radius, self.rect.top - self.settings.dot_radius, self.settings.cellsize - self.settings.dot_radius * 2, self.settings.dot_radius * 2),
+            pygame.Rect(self.rect.right - self.settings.dot_radius, self.rect.top + self.settings.dot_radius, self.settings.dot_radius * 2, self.settings.cellsize - self.settings.dot_radius * 2),
+            pygame.Rect(self.rect.left + self.settings.dot_radius,self.rect.bottom - self.settings.dot_radius, self.settings.cellsize - self.settings.dot_radius * 2, self.settings.dot_radius * 2),
+            pygame.Rect(self.rect.left - self.settings.dot_radius, self.rect.top + self.settings.dot_radius, self.settings.dot_radius * 2, self.settings.cellsize - self.settings.dot_radius * 2)
+            ) #up right down left
 
-        self.line = [False, False, False, False] #up right down left
+        self.line = [None, None, None, None] #up right down left
         self.claim = None #
     
-    def updateLines(self):
+    def updateLines(self) -> None:
         for index, side in enumerate(self.line):
-            if side:
+            if side != None:
                 pygame.draw.line(self.screen, self.line[index], self.corner[index], self.corner[(index+1)% 4], 2)
+            
+            else:
+                pygame.draw.line(self.screen, (210, 210, 210), self.corner[index], self.corner[(index+1)% 4], 2)
         #draw lines
 
-    def displayLineObject(self):
+    def displayLineObject(self) -> None:
         for side in self.line_object:
-            if side:
+            if side != None:
                 pygame.draw.rect(self.screen, (0, 0, 0), side) #display line object
     
-    def checkClaimed(self, current_player):
+    def checkClaimed(self, current_player : Player) -> bool:
         for line in self.line:
-            if not line:
+            if line == None:
                 return False
             
         self.claim = current_player
         return True
     #return true if all line has a value
 
-    def drawCell(self):
+    def drawCell(self, cell_screen : pygame.display) -> None:
         if self.claim:
-            pygame.draw.rect(self.screen, self.claim.color, self.rect) #draw cell object
+            pygame.draw.rect(cell_screen, self.claim.color, self.rect) #draw cell object
+            
                 
-    def cellReset(self):
+    def cellReset(self) -> None:
         self.line = [False, False, False, False]
         self.claim = None
