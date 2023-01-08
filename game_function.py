4a07a85d2b97aabb7ce6aca0abc672526c75371e
Ëzo-Tanuki <michailbryanlie@gmail.com>
2023-01-08 20:07:24 +0700

    add cell screen n more

--- game_function.py
+++ game_function.py
@@ -1,17 +1,26 @@
 import pygame
 import sys
 
-def update_screen(settings, screen, cells, players, stats, button):
+def update_screen(settings, screen, grid, players, stats, button):
     
     screen.fill(settings.bg_color) #background color
-    updateCells(cells) #update all cell
+
+    cell_screen = pygame.Surface((screen.get_width(), screen.get_height()))
+    cell_screen.fill((255, 255, 255))
+    cell_screen.set_colorkey((255, 255, 255))
+    cell_screen.set_alpha(100)
+    updateCells(grid, cell_screen) #update all cell
+    
+    screen.blit(cell_screen, (0, 0))
+
     draw_dot(settings, screen) #draw dots
-    stats.prepScore(players) #render score
+    stats.prepScore() #render score
     stats.showScore() #show score
     if not stats.game_active: #show button if game is not active
         button.drawButton()
+        stats.showWinner()
 
-    pygame.display.flip() #refresh screen
+    pygame.display.update() #refresh screen
 
 def draw_dot(settings, screen):
 
@@ -21,8 +30,8 @@ def draw_dot(settings, screen):
             pygame.draw.circle(screen, settings.dot_color, 
                 (settings.padding_x + column*settings.cellsize, settings.padding_y + row*settings.cellsize), settings.dot_radius)
             
-def showTriggeredCell(cells, pos):
-    for cell in cells:
+def showTriggeredCell(grid, pos):
+    for cell in grid:
         if pos and cell.rect.collidepoint(pos):
             print(cell.rect)
 
@@ -34,17 +43,17 @@ def checkLineObjectTriggered(cell, pos):
     
     return -1
 
-def updateCells(cells):
-    for cell in cells:
-        cell.drawCell() #draw all cell object
+def updateCells(grid, cell_screen):
+    for cell in grid:
+        cell.drawCell(cell_screen) #draw all cell object
         cell.updateLines() #draw line
     
     return None
 
-def checkLineSelected(cells, pos, players, current_player, stats):
+def checkLineSelected(grid, pos, players, current_player, stats):
     #checks line object for each cell is successfully clicked if so change the current player and put the line
     triggered = False
-    for cell in cells:
+    for cell in grid:
         index = checkLineObjectTriggered(cell, pos) 
         '''
         check for any line object triggered and return an index from 
@@ -69,7 +78,7 @@ def checkLineSelected(cells, pos, players, current_player, stats):
 def checkButtonClicked(button, mousePos):
     return button.rect.collidepoint(mousePos)
 
-def checkEvents(settings, screen, cells, players, current_player, stats, button):
+def checkEvents(settings, screen, grid, players, current_player, stats, button):
 
     for event in pygame.event.get(): #lists all event
             if event.type == pygame.QUIT: #close window and program if 'x' is clicked
@@ -79,19 +88,20 @@ def checkEvents(settings, screen, cells, players, current_player, stats, button)
             if event.type == pygame.MOUSEBUTTONDOWN: #save mouse position in an event of mouse click
                 mousePos = event.pos
                 if stats.game_active: #if game in active status start the game
-                    checkLineSelected(cells, mousePos, players, current_player, stats)
+                    checkLineSelected(grid, mousePos, players, current_player, stats)
 
-                    showTriggeredCell(cells, mousePos)
+                    showTriggeredCell(grid, mousePos)
 
                     if stats.unclaimed_cells <= 0:
                         stats.game_active = False
+                        stats.setWinner()
                 
                 else: #otherwise the game will wait the play button to be clicked
                     if checkButtonClicked(button, mousePos):
-                        gameReset(cells, players, stats)
+                        gameReset(grid, players, stats)
 
-def gameReset(cells, players, stats):
-    for cell in cells:
+def gameReset(grid, players, stats):
+    for cell in grid:
         cell.cellReset()
     for player in players:
         player.resetScore()
